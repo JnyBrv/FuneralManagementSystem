a4a6e312d03e380f28e74009070c10a20596cd77
Romwel Aljen Arroyo <arroyo.292197@malolos.sti.edu.ph>
2024-11-21 22:18:12 +0800

    testing

---
+++ FrmLogIn.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FuneralManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //hellow
+            //punta ka ng sharing tapos tapos try mo pindutin session chat
+            //niceeeeeeeeeeeeeeeeeeeeeeeweeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+            // tapusin ko lang ateng yung requirements tas start na tayo bukas 
+            // di na naman ako matutulog 
+            // nakatulog ako kanina eh HAHAH kaya di ko nagawa 
+            // KITA NA BA AKO OMG KAYO
+            //NASAN AKO EMZ
+            //REAL NA BA TO
+            //KITA BA CHAT KO SA LIVE SHARE CHAT?????????????
+            // ouuu kinikilig ako HAHAHHAAHA
+            // finally 
+            // pacheck chat
+
+
+        }
+    }
+}
