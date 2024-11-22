4840e56cd14ed4e7f6f2a2065b3e325dfcef4814
jiannesantos <santosjianneferryll@gmail.com>
2024-11-22 14:29:33 +0800

    jiannef

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace FuneralManagementSystem
         [STAThread]
         static void Main()
         {
-            //Jianne
+            //JianneFerryll
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmLogIn());
