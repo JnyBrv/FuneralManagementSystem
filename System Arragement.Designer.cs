4256c4e5d4980e9c094f02afc6351d53e095e28a
Romwel Aljen Arroyo <arroyo.292197@malolos.sti.edu.ph>
2024-11-22 14:23:48 +0800

    log3

--- System Arragement.Designer.cs
+++ System Arragement.Designer.cs
@@ -33,8 +33,15 @@
             this.SlidingPanel = new System.Windows.Forms.Panel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.btnSlideHide = new System.Windows.Forms.Button();
+            this.TopPanel = new System.Windows.Forms.Panel();
             this.btnExit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.SlidingPanel.SuspendLayout();
+            this.TopPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -44,16 +51,18 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(32, 681);
+            this.panel1.Size = new System.Drawing.Size(32, 578);
             this.panel1.TabIndex = 0;
             // 
             // SlidingPanel
             // 
             this.SlidingPanel.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.SlidingPanel.Controls.Add(this.button2);
+            this.SlidingPanel.Controls.Add(this.button1);
             this.SlidingPanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.SlidingPanel.Location = new System.Drawing.Point(32, 0);
             this.SlidingPanel.Name = "SlidingPanel";
-            this.SlidingPanel.Size = new System.Drawing.Size(114, 681);
+            this.SlidingPanel.Size = new System.Drawing.Size(114, 578);
             this.SlidingPanel.TabIndex = 1;
             // 
             // timer1
@@ -71,10 +80,21 @@
             this.btnSlideHide.UseVisualStyleBackColor = true;
             this.btnSlideHide.Click += new System.EventHandler(this.btnSlideHide_Click);
             // 
+            // TopPanel
+            // 
+            this.TopPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.TopPanel.Controls.Add(this.button4);
+            this.TopPanel.Controls.Add(this.button3);
+            this.TopPanel.Controls.Add(this.btnExit);
+            this.TopPanel.Location = new System.Drawing.Point(146, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(982, 83);
+            this.TopPanel.TabIndex = 3;
+            // 
             // btnExit
             // 
             this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnExit.Location = new System.Drawing.Point(1040, 12);
+            this.btnExit.Location = new System.Drawing.Point(904, 0);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 2;
@@ -82,18 +102,56 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(20, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(103, 28);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // System_Arragement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1127, 681);
-            this.Controls.Add(this.btnExit);
+            this.ClientSize = new System.Drawing.Size(1127, 578);
+            this.Controls.Add(this.TopPanel);
             this.Controls.Add(this.SlidingPanel);
             this.Controls.Add(this.panel1);
             this.Name = "System_Arragement";
             this.Text = "System_Arragement";
             this.Load += new System.EventHandler(this.System_Arragement_Load);
             this.panel1.ResumeLayout(false);
+            this.SlidingPanel.ResumeLayout(false);
+            this.TopPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -104,6 +162,11 @@
         private System.Windows.Forms.Button btnSlideHide;
         private System.Windows.Forms.Panel SlidingPanel;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button btnExit;
     }
 }
