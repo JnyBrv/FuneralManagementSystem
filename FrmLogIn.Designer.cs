b40bdf5c81a235e20f97fda2a1bf954027871573
jiannesantos <santosjianneferryll@gmail.com>
2024-11-29 20:45:28 +0800

    login system sql connection

--- FrmLogIn.Designer.cs
+++ FrmLogIn.Designer.cs
@@ -28,86 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.LogIn = new System.Windows.Forms.Label();
-            this.txtUser = new System.Windows.Forms.TextBox();
-            this.txtPass = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtLogin = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.cbShowPassword = new System.Windows.Forms.CheckBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblWarning = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // LogIn
-            // 
-            this.LogIn.AutoSize = true;
-            this.LogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.LogIn.Location = new System.Drawing.Point(99, 72);
-            this.LogIn.Name = "LogIn";
-            this.LogIn.Size = new System.Drawing.Size(91, 20);
-            this.LogIn.TabIndex = 0;
-            this.LogIn.Text = "Username: ";
-            this.LogIn.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // txtUser
-            // 
-            this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.txtUser.Location = new System.Drawing.Point(103, 95);
-            this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(168, 26);
-            this.txtUser.TabIndex = 1;
-            // 
-            // txtPass
-            // 
-            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.txtPass.Location = new System.Drawing.Point(103, 161);
-            this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(168, 26);
-            this.txtPass.TabIndex = 1;
-            this.txtPass.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label1.Location = new System.Drawing.Point(99, 138);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Password:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // txtLogin
-            // 
-            this.txtLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.txtLogin.Location = new System.Drawing.Point(142, 204);
-            this.txtLogin.Name = "txtLogin";
-            this.txtLogin.Size = new System.Drawing.Size(97, 31);
-            this.txtLogin.TabIndex = 2;
-            this.txtLogin.Text = "Login";
-            this.txtLogin.UseVisualStyleBackColor = true;
-            this.txtLogin.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(354, 12);
+            this.btnExit.Location = new System.Drawing.Point(1017, 13);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(33, 23);
+            this.btnExit.Size = new System.Drawing.Size(44, 28);
             this.btnExit.TabIndex = 3;
             this.btnExit.Text = "X";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // cbShowPassword
+            // 
+            this.cbShowPassword.AutoSize = true;
+            this.cbShowPassword.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbShowPassword.ForeColor = System.Drawing.Color.Gold;
+            this.cbShowPassword.Location = new System.Drawing.Point(604, 292);
+            this.cbShowPassword.Name = "cbShowPassword";
+            this.cbShowPassword.Size = new System.Drawing.Size(149, 24);
+            this.cbShowPassword.TabIndex = 14;
+            this.cbShowPassword.Text = "Show Password";
+            this.cbShowPassword.UseVisualStyleBackColor = true;
+            this.cbShowPassword.CheckedChanged += new System.EventHandler(this.cbShowPassword_CheckedChanged);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Gold;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(384, 378);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(0);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(369, 47);
+            this.btnLogin.TabIndex = 13;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblWarning
+            // 
+            this.lblWarning.AutoSize = true;
+            this.lblWarning.Font = new System.Drawing.Font("Georgia", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWarning.ForeColor = System.Drawing.Color.Red;
+            this.lblWarning.Location = new System.Drawing.Point(380, 331);
+            this.lblWarning.Name = "lblWarning";
+            this.lblWarning.Size = new System.Drawing.Size(367, 20);
+            this.lblWarning.TabIndex = 12;
+            this.lblWarning.Text = "Wrong username or password. Please try again.";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(384, 235);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(369, 34);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(384, 165);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(369, 34);
+            this.txtUsername.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gold;
+            this.label3.Location = new System.Drawing.Point(234, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(234, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Username:";
+            // 
             // frmLogIn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(399, 309);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1074, 571);
             this.ControlBox = false;
+            this.Controls.Add(this.cbShowPassword);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblWarning);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.txtLogin);
-            this.Controls.Add(this.txtPass);
-            this.Controls.Add(this.txtUser);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.LogIn);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmLogIn";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Log in";
@@ -118,13 +151,14 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label LogIn;
-        private System.Windows.Forms.TextBox txtUser;
-        private System.Windows.Forms.TextBox txtPass;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button txtLogin;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.CheckBox cbShowPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblWarning;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- FrmLogIn.cs
+++ FrmLogIn.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace FuneralManagementSystem
 {
@@ -15,13 +17,14 @@ namespace FuneralManagementSystem
         public frmLogIn()
         {
             InitializeComponent();
+
             
+            lblWarning.Hide(); //Wrong login credentials warning.
+            txtPassword.PasswordChar = '•'; //To hide password letters.
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
+        //SQL Connection
+        SqlConnection con = new SqlConnection(@"Data Source=JIANNESANTOS\SQLEXPRESS;Initial Catalog=FuneralManagementSystem;Integrated Security=True");
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -29,21 +32,82 @@ namespace FuneralManagementSystem
             WindowState = FormWindowState.Normal;
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            System_Arragement sysArr = new System_Arragement();
-            sysArr.ShowDialog();
-            this.Close();
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
-            Close();
+            //Confirmation message dialog
+            DialogResult result = MessageBox.Show("Are you sure you want to close the system? ", 
+                "Exit Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Close();
+            }
+            
+        }
+
+        //Login button
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            String username = txtUsername.Text;
+            String password = txtPassword.Text;
+
+            try
+            {
+                con.Open();
+                String query = "SELECT * FROM ACCOUNTS WHERE userName = '" + username +
+                    "' AND userPassword = '" + password + "'";
+
+                SqlDataAdapter sda = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    //Open Home form
+                    System_Arragement sysar = new System_Arragement();
+                    sysar.Show();
+                    this.Hide();
+
+                    //Login History
+                    String log = "LoginHistory.txt";
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = con;
+                    cmd.CommandText = "SELECT userId FROM ACCOUNTS WHERE userName = '" + username +
+                                            "' AND userPassword = '" + password + "' ";
+
+                    SqlDataReader read = cmd.ExecuteReader();
+                    while (read.Read())
+                    {
+                        StreamWriter write = File.AppendText(log);
+                        write.WriteLine("ID: " + read[0] + ", NAME: " + username + ", DATE: " + DateTime.Now.ToString());
+                        write.Close();
+                    }
+                }
+                else
+                {
+                    lblWarning.Show();
+                    txtPassword.Clear();
+                    txtUsername.Clear();
+                }
+            }
+            catch
+            {
+                lblWarning.Show();
+                txtPassword.Clear();
+                txtUsername.Clear();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        //Show Password checkbox
+        private void cbShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPassword.PasswordChar = cbShowPassword.Checked ? '\0' : '•';
         }
     }
 }
