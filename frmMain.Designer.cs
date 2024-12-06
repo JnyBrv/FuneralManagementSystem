bdc554ec6f9fad0fb747a6e3a312f421a0389e8e
Joshua Castro <castro.277027@malolos.sti.edu.ph>
2024-12-06 20:47:21 +0800

    joshua

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -28,22 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.pnlBrand = new System.Windows.Forms.Panel();
+            this.panelMainMenu = new System.Windows.Forms.Panel();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.pbExit = new System.Windows.Forms.PictureBox();
             this.btnEmployees = new System.Windows.Forms.Button();
             this.btnClients = new System.Windows.Forms.Button();
             this.btnPackages = new System.Windows.Forms.Button();
-            this.panelMainMenu = new System.Windows.Forms.Panel();
-            this.panelTitleBar = new System.Windows.Forms.Panel();
-            this.pnlBrand = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pbExit = new System.Windows.Forms.PictureBox();
-            this.btnLogout = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.pnlBrand.SuspendLayout();
             this.panelMainMenu.SuspendLayout();
             this.panelTitleBar.SuspendLayout();
-            this.pnlBrand.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbExit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -56,10 +57,70 @@
             this.panel1.Controls.Add(this.pnlBrand);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(307, 1026);
+            this.panel1.Size = new System.Drawing.Size(230, 834);
             this.panel1.TabIndex = 0;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.Black;
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.Color.Gold;
+            this.btnLogout.Image = global::FuneralManagementSystem.Properties.Resources.backIcon;
+            this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.Location = new System.Drawing.Point(0, 769);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(231, 54);
+            this.btnLogout.TabIndex = 2;
+            this.btnLogout.Text = "Log Out";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // pnlBrand
+            // 
+            this.pnlBrand.BackColor = System.Drawing.Color.Black;
+            this.pnlBrand.Controls.Add(this.pictureBox1);
+            this.pnlBrand.Location = new System.Drawing.Point(0, 0);
+            this.pnlBrand.Name = "pnlBrand";
+            this.pnlBrand.Size = new System.Drawing.Size(230, 184);
+            this.pnlBrand.TabIndex = 0;
+            // 
+            // panelMainMenu
+            // 
+            this.panelMainMenu.AutoScroll = true;
+            this.panelMainMenu.Controls.Add(this.panelTitleBar);
+            this.panelMainMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMainMenu.Location = new System.Drawing.Point(230, 0);
+            this.panelMainMenu.Name = "panelMainMenu";
+            this.panelMainMenu.Size = new System.Drawing.Size(1213, 834);
+            this.panelMainMenu.TabIndex = 1;
+            this.panelMainMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.panelMainMenu_Paint);
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.Black;
+            this.panelTitleBar.Controls.Add(this.pbExit);
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(1213, 64);
+            this.panelTitleBar.TabIndex = 0;
+            // 
+            // pbExit
+            // 
+            this.pbExit.BackColor = System.Drawing.Color.Black;
+            this.pbExit.Image = global::FuneralManagementSystem.Properties.Resources.exit;
+            this.pbExit.Location = new System.Drawing.Point(1175, 2);
+            this.pbExit.Margin = new System.Windows.Forms.Padding(2);
+            this.pbExit.Name = "pbExit";
+            this.pbExit.Size = new System.Drawing.Size(38, 41);
+            this.pbExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbExit.TabIndex = 16;
+            this.pbExit.TabStop = false;
+            this.pbExit.Click += new System.EventHandler(this.pbExit_Click);
             // 
             // btnEmployees
             // 
@@ -69,12 +130,13 @@
             this.btnEmployees.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnEmployees.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEmployees.ForeColor = System.Drawing.Color.Gold;
-            this.btnEmployees.Location = new System.Drawing.Point(-23, 463);
-            this.btnEmployees.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEmployees.Image = ((System.Drawing.Image)(resources.GetObject("btnEmployees.Image")));
+            this.btnEmployees.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEmployees.Location = new System.Drawing.Point(0, 376);
             this.btnEmployees.Name = "btnEmployees";
-            this.btnEmployees.Size = new System.Drawing.Size(331, 66);
+            this.btnEmployees.Size = new System.Drawing.Size(231, 54);
             this.btnEmployees.TabIndex = 1;
-            this.btnEmployees.Text = "Employees";
+            this.btnEmployees.Text = "   Employees";
             this.btnEmployees.UseVisualStyleBackColor = false;
             this.btnEmployees.Click += new System.EventHandler(this.btnEmployees_Click);
             // 
@@ -86,10 +148,11 @@
             this.btnClients.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClients.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClients.ForeColor = System.Drawing.Color.Gold;
-            this.btnClients.Location = new System.Drawing.Point(-16, 376);
-            this.btnClients.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClients.Image = ((System.Drawing.Image)(resources.GetObject("btnClients.Image")));
+            this.btnClients.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClients.Location = new System.Drawing.Point(0, 306);
             this.btnClients.Name = "btnClients";
-            this.btnClients.Size = new System.Drawing.Size(323, 66);
+            this.btnClients.Size = new System.Drawing.Size(230, 54);
             this.btnClients.TabIndex = 1;
             this.btnClients.Text = "Clients";
             this.btnClients.UseVisualStyleBackColor = false;
@@ -103,106 +166,45 @@
             this.btnPackages.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnPackages.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPackages.ForeColor = System.Drawing.Color.Gold;
-            this.btnPackages.Location = new System.Drawing.Point(-23, 290);
-            this.btnPackages.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPackages.Image = global::FuneralManagementSystem.Properties.Resources.packageIcon;
+            this.btnPackages.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPackages.Location = new System.Drawing.Point(0, 236);
             this.btnPackages.Name = "btnPackages";
-            this.btnPackages.Size = new System.Drawing.Size(331, 66);
+            this.btnPackages.Size = new System.Drawing.Size(231, 54);
             this.btnPackages.TabIndex = 1;
             this.btnPackages.Text = "Packages";
             this.btnPackages.UseVisualStyleBackColor = false;
             this.btnPackages.Click += new System.EventHandler(this.button1_Click);
             // 
-            // panelMainMenu
-            // 
-            this.panelMainMenu.AutoScroll = true;
-            this.panelMainMenu.Controls.Add(this.panelTitleBar);
-            this.panelMainMenu.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelMainMenu.Location = new System.Drawing.Point(307, 0);
-            this.panelMainMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panelMainMenu.Name = "panelMainMenu";
-            this.panelMainMenu.Size = new System.Drawing.Size(1617, 1026);
-            this.panelMainMenu.TabIndex = 1;
-            this.panelMainMenu.Paint += new System.Windows.Forms.PaintEventHandler(this.panelMainMenu_Paint);
-            // 
-            // panelTitleBar
-            // 
-            this.panelTitleBar.BackColor = System.Drawing.Color.Black;
-            this.panelTitleBar.Controls.Add(this.pbExit);
-            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTitleBar.Location = new System.Drawing.Point(0, 0);
-            this.panelTitleBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panelTitleBar.Name = "panelTitleBar";
-            this.panelTitleBar.Size = new System.Drawing.Size(1617, 79);
-            this.panelTitleBar.TabIndex = 0;
-            // 
-            // pnlBrand
-            // 
-            this.pnlBrand.BackColor = System.Drawing.Color.Black;
-            this.pnlBrand.Controls.Add(this.pictureBox1);
-            this.pnlBrand.Location = new System.Drawing.Point(0, 0);
-            this.pnlBrand.Margin = new System.Windows.Forms.Padding(4);
-            this.pnlBrand.Name = "pnlBrand";
-            this.pnlBrand.Size = new System.Drawing.Size(307, 226);
-            this.pnlBrand.TabIndex = 0;
-            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::FuneralManagementSystem.Properties.Resources.cruzlogo;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(307, 223);
+            this.pictureBox1.Size = new System.Drawing.Size(230, 181);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // pbExit
-            // 
-            this.pbExit.BackColor = System.Drawing.Color.Black;
-            this.pbExit.Image = global::FuneralManagementSystem.Properties.Resources.exit;
-            this.pbExit.Location = new System.Drawing.Point(1567, 3);
-            this.pbExit.Name = "pbExit";
-            this.pbExit.Size = new System.Drawing.Size(50, 50);
-            this.pbExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pbExit.TabIndex = 16;
-            this.pbExit.TabStop = false;
-            this.pbExit.Click += new System.EventHandler(this.pbExit_Click);
-            // 
-            // btnLogout
-            // 
-            this.btnLogout.BackColor = System.Drawing.Color.Black;
-            this.btnLogout.FlatAppearance.BorderSize = 0;
-            this.btnLogout.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
-            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogout.Font = new System.Drawing.Font("Georgia", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogout.ForeColor = System.Drawing.Color.Gold;
-            this.btnLogout.Location = new System.Drawing.Point(-16, 947);
-            this.btnLogout.Margin = new System.Windows.Forms.Padding(4);
-            this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(331, 66);
-            this.btnLogout.TabIndex = 2;
-            this.btnLogout.Text = "Log Out";
-            this.btnLogout.UseVisualStyleBackColor = false;
-            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
-            // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1924, 1026);
+            this.ClientSize = new System.Drawing.Size(1443, 834);
             this.Controls.Add(this.panelMainMenu);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMain";
             this.Text = "frmMain";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.panel1.ResumeLayout(false);
+            this.pnlBrand.ResumeLayout(false);
             this.panelMainMenu.ResumeLayout(false);
             this.panelTitleBar.ResumeLayout(false);
-            this.pnlBrand.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbExit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
