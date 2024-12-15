97da22d8c277455d724e14e8efc6d5774f0efc7e
Romwel Aljen Arroyo <arroyo.292197@malolos.sti.edu.ph>
2024-12-16 02:28:03 +0800

    DeleteButton

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace FuneralManagementSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
          
-            Application.Run(new FrmPackages());
+            Application.Run(new frmLogIn());
             
         }
 
